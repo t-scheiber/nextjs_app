4066467867e8b9aa7dd85847a1e3f52d6981973e
Thomas Scheiber <66697291+t-scheiber@users.noreply.github.com>
2024-01-21 13:46:27 +0100

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
 import "../styles/Home.module.css";
 import "../styles/globals.css";
-import Particles from '../components/Particles.js';
+import ParticlesComponent from '../components/Particles.js';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      <Particles />
+      <ParticlesComponent />
     </>
   );
 }
