5ad159b28eb576c8b0e8b364ae76893ddc6e5296
Thomas Scheiber <66697291+t-scheiber@users.noreply.github.com>
2024-01-28 06:14:50 +0100

    logos added

--- pages/certifications.js
+++ pages/certifications.js
@@ -31,10 +31,80 @@ export default function Home() {
         <link rel="manifest" href="/files/favicons/site.webmanifest" />
       </Head>
       <div className={styles.container}>
-        <ul  className={styles.liste}>
-          <li>
-
-          </li>
+        <div className={styles.heading}>Certificates</div>
+        <ul className={styles.liste}>
+          <a href="/files/Meta-Front-End-Dev-Certificate.pdf">
+            <li className={styles.eintrag}>
+              <span className={styles.logos}>
+                <img className={styles.images} alt=" Logo" src="/files/meta.png" />
+              </span>
+              Meta Front-End Developer
+            </li>
+          </a>
+          <a href="/files/AWS_Certificate.pdf">
+            <li className={styles.eintrag}>
+              <span className={styles.logos}>
+                <img className={styles.images} alt=" Logo" src="/files/aws.png" />
+              </span>
+              AWS Cloud Solutions Architect
+            </li>
+          </a>
+          <a href="/files/IT-Automation-Python_Certificate.pdf">
+            <li className={styles.eintrag}>
+              <span className={styles.logos}>
+                <img className={styles.images} alt=" Logo" src="/files/google.png" />
+              </span>
+              Google IT Automation with Python
+            </li>
+          </a>
+          <a href="/files/IT-Support_Certificate.pdf">
+            <li className={styles.eintrag}>
+              <span className={styles.logos}>
+                <img className={styles.images} alt=" Logo" src="/files/google.png" />
+              </span>
+              Google IT Support
+            </li>
+          </a>
+          <a href="/files/EF_Certificate.pdf">
+            <li className={styles.eintrag}>
+              <span className={styles.logos}>
+                <img className={styles.images} alt=" Logo" src="/files/ef.png" />
+              </span>
+              EF Language School C1
+            </li>
+          </a>
+          <a href="/files/FCE_Certificate.pdf">
+            <li className={styles.eintrag}>
+              <span className={styles.logos}>
+                <img className={styles.images} alt=" Logo" src="/files/cambridge.png" />
+              </span>
+              Cambridge English: First
+            </li>
+          </a>
+          <a href="/files/ECDL.pdf">
+            <li className={styles.eintrag}>
+              <span className={styles.logos}>
+                <img className={styles.images} alt=" Logo" src="/files/ecdl.png" />
+              </span>
+              ECDL
+            </li>
+          </a>
+          <a href="/files/makeITsafe.pdf">
+            <li className={styles.eintrag}>
+              <span className={styles.logos}>
+                <img className={styles.images} alt=" Logo" src="/files/ecpat.png" />
+              </span>
+              make IT safe<sup>2.0</sup>
+            </li>
+          </a>
+          <a href="/files/Italian_Language_Certificate.pdf">
+            <li className={styles.eintrag}>
+              <span className={styles.logos}>
+                <img className={styles.images} alt=" Logo" src="/files/scuolaLdV.png" />
+              </span>
+              Italian Language B1
+            </li>
+          </a>
         </ul>
         <Footer />
       </div>
