991922ad812a24142b05e9a1d83d0b9dc71682bc
Thomas Scheiber <66697291+t-scheiber@users.noreply.github.com>
2024-01-28 07:38:44 +0100

    added vercel speed insights

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "../styles/Home.module.css";
 import "../styles/globals.css";
 import ParticlesComponent from '../components/Particles.js';
@@ -5,6 +6,7 @@ import ParticlesComponent from '../components/Particles.js';
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <SpeedInsights/>
       <Component {...pageProps} />
       <ParticlesComponent />
     </>
