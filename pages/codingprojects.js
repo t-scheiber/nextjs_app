fae4521bc11ce6810b75f65668ae1f4ad98e9d8f
Thomas Scheiber <66697291+t-scheiber@users.noreply.github.com>
2024-02-13 18:52:43 +0100

    added new Project

--- pages/codingprojects.js
+++ pages/codingprojects.js
@@ -42,6 +42,12 @@ export default function Home() {
             <a className={styles.links} href="https://todo-app-tscheiber.vercel.app/">
               <li className={styles.eintrag}>Todo App</li>
             </a>
+            <a className={styles.links} href="https://advanced-react-course-portfolio.vercel.app/">
+              <li className={styles.eintrag}>React Advanced Course: Portfolio App</li>
+            </a>
+            <a className={styles.links} href="https://react-basics-simplest-working-calculator-rho.vercel.app/">
+              <li className={styles.eintrag}>React Basics Course: Simplest working Calculator App</li>
+            </a>
             <a className={styles.links} href="https://t-scheiber.github.io/CalculatorApp">
               <li className={styles.eintrag}>Calculator App</li>
             </a>
