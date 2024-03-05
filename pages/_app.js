b30f2b553abefdeaf877786311c0279080aa0be7
Thomas Scheiber <66697291+t-scheiber@users.noreply.github.com>
2024-03-05 23:50:40 +0100

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "../styles/Home.module.css";
 import "../styles/globals.css";
-import ParticlesComponent from '../components/Particles.js';
+import ParticlesComponent from "../components/Particles.js";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      <SpeedInsights/>
+      <Analytics />
+      <SpeedInsights />
       <Component {...pageProps} />
       <ParticlesComponent />
     </>
