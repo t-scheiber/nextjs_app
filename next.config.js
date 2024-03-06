c993b3066ae7a11a20edd2eb693eae62823590e1
Thomas Scheiber <66697291+t-scheiber@users.noreply.github.com>
2024-03-06 14:43:20 +0100

    config file changes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
